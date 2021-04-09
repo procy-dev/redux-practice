af805846476b25d0b39fe0b15f781e01f489023e
Chris Winkler <procy.dev@gmail.com>
2021-04-09 13:18:20 -0700

    BrowserRouter and Switch implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import HomePage from './Components/HomePage/HomePage'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div>
-            <HomePage />
-        </div>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route>404 Page Not Found</Route>
+                </Switch>
+                
+            </div>
+        </Router>
+        
     );
 }
 
